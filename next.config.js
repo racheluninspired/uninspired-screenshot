381d3592d87ff24dc03d110aba060d5d168caa2c
racheluninspired <rachel@uninspiredcollective.com>
2026-05-07 13:47:47 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  serverExternalPackages: ['@sparticuz/chromium-min', 'puppeteer-core'],
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
